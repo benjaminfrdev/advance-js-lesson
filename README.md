da67b2632fee3f27c6501fbfc71ac4b4e9dcd6ba
benjaminfrdev <103106842+benjaminfrdev@users.noreply.github.com>
2022-09-07 15:58:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-qr1fxx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-qr1fxx)
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const student = {
+  name: 'Minh Khoa',
+};
+
+student.name = 'abc';
+
+console.log(student);
